ad9835de3b4900f24d5109d742563bc53824a19e
Rohul1 <rohul669319@gmail.com>
2023-12-21 16:13:01 +0600

    updated

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = ({product, handleAddTocart}) => {
@@ -15,7 +17,8 @@ const Product = ({product, handleAddTocart}) => {
            <p><small>Ratings :{ratings} stars</small></p>
          </div>
          <button onClick={()=>handleAddTocart(product)} className='btn-cart'>
-            <p>Add To Cart</p>
+            <p className='btn-text'>Add To Cart</p>
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
          </button>
         </div>
     );
